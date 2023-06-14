bf9e9e53cfe682ad794ffb93b92176e04191e55b
TJ Klimoski <tjklimoski@gmail.com>
2023-06-14 14:59:22 -0400

    Setup routes on client with react-router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Shop from "./pages/Shop"
+import About from "./pages/About"
 
 function App() {
 
-  return <h1>Hello</h1>
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/shop" element={<Shop />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </>
+    )
 }
 
 export default App
