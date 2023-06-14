02628deb85444796fe2f1dafa8c6a7c0ff35ec97
TJ Klimoski <tjklimoski@gmail.com>
2023-06-14 17:20:29 -0400

    Style Navbar

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -2,16 +2,23 @@ import { Link } from "react-router-dom";
 import { MdShoppingCart } from 'react-icons/md'
 
 export default function Nav() {
+  const cartQty = 5;
+
   return (
     <nav id="navbar">
       <h1 className="company-name">Chocolate</h1>
-      <div>
+      <div className="links-wrapper">
         <Link className="nav-link" to="/">Home</Link>
         <Link className="nav-link" to="/shop">Shop</Link>
         <Link className="nav-link" to="/about">About</Link>
       </div>
       <button className="shopping-cart-btn">
         <MdShoppingCart />
+        { cartQty > 0 &&
+          <div className="cart-qty">
+            {cartQty}
+          </div>
+        }
       </button>
     </nav>
   )
